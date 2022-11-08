46d96ac65a218ed48d8430ebf1e675a56bbbab04
thanawatsa <76862711+thanawatsa@users.noreply.github.com>
2022-11-09 04:24:45 +0700

    check diceImage is not null

--- app/src/main/java/com/nantial/diceroller/MainActivity.kt
+++ app/src/main/java/com/nantial/diceroller/MainActivity.kt
@@ -10,6 +10,7 @@ import org.w3c.dom.Text
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = "Let's Roll!"
         rollButton.setOnClickListener {
             rollDice()
-
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -32,12 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
-
-
-
-
-
     }
 }
