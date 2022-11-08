f7c5ff7f9e7b1170d763964733f2f9f82f85db2a
thanawatsa <76862711+thanawatsa@users.noreply.github.com>
2022-11-09 04:13:21 +0700

    Add dice image

--- app/src/main/java/com/nantial/diceroller/MainActivity.kt
+++ app/src/main/java/com/nantial/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nantial.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
@@ -22,9 +23,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        var resultText : TextView = findViewById(R.id.result_text)
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
+
 
 
 
